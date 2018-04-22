0ee85d533fbd35280bfe1451a69398b0a3ab0f82
朱允见 <719307805@qq.com>
2018-04-22 13:30:39 +0800

    初始化

---
+++ Classes/UIColor+Hex.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+Hex.h
+//  myDemo
+//
+//  Created by apple on 16/9/6.
+//  Copyright © 2016年 lv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor(Hex)
++ (UIColor *)colorWithHexString:(NSString *)color alpha:(CGFloat)alpha;
+@end
